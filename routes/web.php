4f523fd6c3f0e80faf95b3384242301d2d94a0d2
Yuta-alt <wxrmtr9c@gmail.com>
2020-05-01 05:13:22 +0000

    larabel_13

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,12 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::post('news/create', 'Admin\NewsController@create');
     
     Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
     Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+    Route::post('profile/create', 'Admin\ProfileController@create');
+    Route::post('profile/edit', 'Admin\ProfileController@update');
 });
 
 Auth::routes();
