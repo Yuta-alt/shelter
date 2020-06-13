b98f6e2f9d93c2d39825f61fd5036bbfde39e1a2
Yuta-alt <wxrmtr9c@gmail.com>
2020-06-13 10:10:25 +0000

    0601３

--- shelter_list/text_array1.php
+++ shelter_list/text_array1.php
@@ -1,26 +1,27 @@
 <?php
-$file = fopen("Mie_write.csv", "r+");
+// ↓架空のファイルを開く（＝新規のファイル作成）
+$file = fopen("Mie_write.csv", "w");
+// ↓参照するファイル名
 $filename = "Mie_write.txt";
 $array = file($filename); //配列
-//or
-// $strGet = file_get_contents($filename); //文字列
-
 $i = 0;
-while(count($array) > $i){
+// ↓ 「Mie_write.txt の最後の行まで」と言う条件指定
+for ($i = 0 ; $i <= count($array);$i++){
+// ↓１行・２行・３行の読み込み
+  // ↓改行の削除（配列の連結）
+  $name = $array[$i*3+0];
+    $name = str_replace(PHP_EOL, '', $name);
+  $city = $array[$i*3+1];
+    $city = str_replace(PHP_EOL, '', $city);
+  $address = $array[$i*3+2];
 
-  //下の処理はfileメソッドを使う想定
-  $name = $array[$i]; //←「避難所名A」
-  $city = $array[$i+1];
-  $address = $array[$i+2];
-
-  // $ar = array($name, $shichoson, $jusho);
-  // $str_result = implode(",", $ar);
-  
   //下記をファイルに書き込む
   $contents .= $name.",".$city.",".$address;
 
-  $i .= 3;
+  // $i .= 3;
 }
+fclose($file);
+
 $write = fopen("Mie_write.csv", "w");
 fwrite($write, $contents);
 fclose($write);
